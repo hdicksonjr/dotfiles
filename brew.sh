f237813af80a641f773d75dfe8dab23c4e2b65a0
Henry Dickson <hdicksonjr@Henrys-MacBook-Pro.local>
2016-06-07 09:00:12 -0400

    add brew and osx files

---
+++ brew.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install tools using homebrew.
+
+brew update
+
+# upgrade any installed packages
+brew upgrade -all
+
+brew install coreutils
+brew install vim
+brew install ack
+brew install openssl
+brew install postgresql
+brew install rbenv
+brew install ruby-build
+brew install the_silver_searcher
+
+
